88c2c963b5462c733f15078478f4edae173e0137
Nicky Semenza <nicky@nickysemenza.com>
2021-02-12 18:22:07 -0800

    vulgar function parsing part 1

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 This leverages [nom](https://github.com/Geal/nom) to parse ingredient line items from recipes into a common format.
 
 *wip*
+
+
+example: `1¼  cups / 155.5 grams flour, lightly sifted` => `{ name: "flour", amounts: [Amount { unit: "cups", value: 1.25 }, Amount { unit: "grams", value: 155.5 }], modifier: Some("lightly sifted") }`
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 use nom::{
     branch::alt,
-    bytes::complete::{ tag},
-    character::complete::{alpha1,  space0, space1},
+    bytes::complete::tag,
+    character::complete::{alpha1, satisfy, space0, space1},
     combinator::opt,
     error::context,
     multi::{many0, many1},
     number::complete::float,
-    sequence::{ tuple},
+    sequence::tuple,
 };
 
 extern crate nom;
@@ -62,7 +62,7 @@ pub fn ingredient(input: &str) -> nom::IResult<&str, Ingredient> {
             next_input,
             Ingredient {
                 name: name_chunks.join(""),
-                amounts: amounts,
+                amounts,
                 modifier: match m.chars().count() {
                     0 => None,
                     _ => Some(m),
@@ -72,7 +72,7 @@ pub fn ingredient(input: &str) -> nom::IResult<&str, Ingredient> {
     })
 }
 
-// parses 2 amounts, seperated by ; or /  
+// parses 2 amounts, seperated by ; or /
 fn amount2(input: &str) -> nom::IResult<&str, Vec<Amount>> {
     context(
         "amount2",
@@ -86,9 +86,14 @@ fn amount2(input: &str) -> nom::IResult<&str, Vec<Amount>> {
 
 // parses a single amount
 fn amount1(input: &str) -> nom::IResult<&str, Vec<Amount>> {
-    context("amount1", tuple((float, space0, alpha1)))(input).map(|(next_input, res)| {
+    context(
+        "amount1",
+        tuple(
+            (num, space0, alpha1), // 1 gram
+        ),
+    )(input)
+    .map(|(next_input, res)| {
         let (value, _, unit) = res;
-        println!("foo{:?}", res);
         (
             next_input,
             vec![Amount {
@@ -99,9 +104,82 @@ fn amount1(input: &str) -> nom::IResult<&str, Vec<Amount>> {
     })
 }
 
+pub fn v_frac_to_num(input: &char) -> Result<f32, String> {
+    let (n, d): (i32, i32) = match input {
+        '¾' => (3, 4),
+        '⅛' => (1, 8),
+        '¼' => (1, 4),
+        '⅓' => (1, 3),
+        '½' => (1, 2),
+        _ => return Err(format!("unkown fraction: {}", input)),
+    };
+    return Ok(n as f32 / d as f32);
+}
+
+/// parses unicode vulgar fractions
+pub fn v_fraction(input: &str) -> nom::IResult<&str, char> {
+    context(
+        "v_fraction",
+        satisfy(|c|
+            // two ranges for unicode fractions
+            // https://www.compart.com/en/unicode/search?q=vulgar+fraction#characters
+            (c <= '¾' && c >= '¼') || (c >= '⅐' && c <= '⅞')),
+    )(input)
+}
+
+/// handles vulgar fraction, or just a number
+pub fn num(input: &str) -> nom::IResult<&str, f32> {
+    context("num", alt((fraction_number, float)))(input)
+}
+/// parses `1 ⅛` into 1.125
+pub fn fraction_number(input: &str) -> nom::IResult<&str, f32> {
+    context(
+        "fraction_number",
+        tuple((
+            opt(tuple((float, space0))), // optional number (and if number, optional space) before
+            v_fraction,                  // vulgar frac
+        )),
+    )(input)
+    .map(|(next_input, res)| {
+        let (num, frac) = res;
+        let num1 = match num {
+            Some(x) => x.0,
+            None => 0.0,
+        };
+        let num2 = match v_frac_to_num(&frac) {
+            Ok(x) => x,
+            _ => 0.0,
+        };
+        (next_input, num1 + num2)
+    })
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_fraction() {
+        assert_eq!(fraction_number("1 ⅛"), Ok(("", 1.125)));
+        assert_eq!(fraction_number("1⅓"), Ok(("", 1.3333334)));
+        assert_eq!(fraction_number("¼"), Ok(("", 0.25)));
+        assert_eq!(fraction_number("⅐"), Ok(("", 0.0))); // unkown are dropped
+        assert_eq!(
+            fraction_number("1"),
+            Err(nom::Err::Error(nom::error::Error::new(
+                "",
+                nom::error::ErrorKind::Satisfy
+            )))
+        );
+        // assert_eq!(strip_frac("1 ⅛"), "a");
+    }
+    #[test]
+    fn test_v_fraction() {
+        assert_eq!(v_frac_to_num(&'⅛'), Ok(0.125));
+        assert_eq!(v_frac_to_num(&'¼'), Ok(0.25));
+        assert_eq!(v_frac_to_num(&'½'), Ok(0.5));
+        // assert_eq!(strip_frac("1 ⅛"), "a");
+    }
 
     #[test]
     fn test_ingredient_parse() {
@@ -120,11 +198,11 @@ mod tests {
             ))
         );
         assert_eq!(
-            ingredient("12 cups flour, lightly sifted"),
+            ingredient("12 cups all purpose flour, lightly sifted"),
             Ok((
                 "",
                 Ingredient {
-                    name: "flour".to_string(),
+                    name: "all purpose flour".to_string(),
                     amounts: vec![Amount {
                         unit: "cups".to_string(),
                         value: 12.0
@@ -133,9 +211,9 @@ mod tests {
                 }
             ))
         );
-      
+
         assert_eq!(
-            ingredient("12 cups / 2.3 grams flour"),
+            ingredient("1¼  cups / 155.5 grams flour"),
             Ok((
                 "",
                 Ingredient {
@@ -143,11 +221,11 @@ mod tests {
                     amounts: vec![
                         Amount {
                             unit: "cups".to_string(),
-                            value: 12.0
+                            value: 1.25
                         },
                         Amount {
                             unit: "grams".to_string(),
-                            value: 2.3
+                            value: 155.5
                         }
                     ],
                     modifier: None,
