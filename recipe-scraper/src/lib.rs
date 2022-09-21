a1e9d6bb9f1c4a6c3bfc2e00f105004e0b2f73a4
Nicky Semenza <nicky@nickysemenza.com>
2022-09-21 11:39:09 -0700

    scraper pt 3

--- recipe-scraper/src/lib.rs
+++ recipe-scraper/src/lib.rs
@@ -1,6 +1,7 @@
 use scraper::{Html, Selector};
 
 use serde_json::Value;
+mod http_utils;
 mod ld_schema;
 
 use thiserror::Error;
@@ -13,10 +14,8 @@ pub enum ScrapeError {
     NoLDJSON(String),
     #[error("could not deserialize `{0}`")]
     Deserialize(#[from] serde_json::Error),
-    // #[error("invalid header (expected {expected:?}, found {found:?})")]
-    // InvalidHeader { expected: String, found: String },
-    // #[error("unknown data store error")]
-    // Unknown,
+    #[error("could not parse `{0}`")]
+    Parse(String),
 }
 #[derive(Debug)]
 pub struct ScrapedRecipe {
@@ -30,14 +29,44 @@ pub struct ScrapedRecipe {
 // https://github.com/pombadev/sunny/blob/main/src/lib/spider.rs
 // https://github.com/megametres/recettes-api/blob/dev/src/html_parser/mod.rs
 
-pub fn scrape_url(url: &str) -> Result<ScrapedRecipe, ScrapeError> {
-    let body = fetch_html(url).unwrap();
-    scrape(body.as_ref(), url)
+#[derive(Debug)]
+pub struct Scraper {
+    client: reqwest_middleware::ClientWithMiddleware,
 }
-pub fn scrape(body: &str, url: &str) -> Result<ScrapedRecipe, ScrapeError> {
-    let dom = Html::parse_document(body);
-    let ld_schema = parse(dom)?;
-    Ok(ScrapedRecipe {
+impl Scraper {
+    pub fn new() -> Self {
+        return Scraper {
+            client: http_utils::http_client(),
+        };
+    }
+    #[tracing::instrument(name = "scrape_url")]
+    pub async fn scrape_url(&self, url: &str) -> Result<ScrapedRecipe, ScrapeError> {
+        let body = self.fetch_html(url).await?;
+        self.scrape(body.as_ref(), url)
+    }
+    pub fn scrape(&self, body: &str, url: &str) -> Result<ScrapedRecipe, ScrapeError> {
+        let dom = Html::parse_document(body);
+        let ld_schema = parse(dom)?;
+        Ok(normalize_ld_json(ld_schema, url))
+    }
+    #[tracing::instrument]
+    async fn fetch_html(&self, url: &str) -> Result<String, ScrapeError> {
+        let body = match self.client.get(url).send().await {
+            Ok(r) => r.text().await?,
+            Err(e) => {
+                return Err(match e {
+                    reqwest_middleware::Error::Middleware(e) => panic!("{}", e),
+                    reqwest_middleware::Error::Reqwest(e) => ScrapeError::Http(e),
+                })
+            }
+        };
+        Ok(body)
+    }
+}
+
+#[tracing::instrument]
+fn normalize_ld_json(ld_schema: ld_schema::LDJSONRoot, url: &str) -> ScrapedRecipe {
+    ScrapedRecipe {
         ingredients: ld_schema.recipe_ingredient,
         instructions: ld_schema
             .recipe_instructions
@@ -57,19 +86,13 @@ pub fn scrape(body: &str, url: &str) -> Result<ScrapedRecipe, ScrapeError> {
             },
             None => None,
         },
-    })
-}
-
-fn fetch_html(url: &str) -> Result<String, ScrapeError> {
-    let resp = match reqwest::blocking::get(url) {
-        Ok(r) => r,
-        Err(e) => return Err(ScrapeError::Http(e)),
-    };
-    let body = resp.text()?;
-    Ok(body)
+    }
 }
 fn parse(dom: Html) -> Result<ld_schema::LDJSONRoot, ScrapeError> {
-    let selector = Selector::parse("script[type='application/ld+json']").unwrap();
+    let selector = match Selector::parse("script[type='application/ld+json']") {
+        Ok(s) => s,
+        Err(e) => return Err(ScrapeError::Parse(format!("{:?}", e))),
+    };
 
     let element = match dom.select(&selector).next() {
         Some(e) => e,
@@ -81,7 +104,8 @@ fn parse(dom: Html) -> Result<ld_schema::LDJSONRoot, ScrapeError> {
 }
 fn parse_ld_json(json: String) -> Result<ld_schema::LDJSONRoot, ScrapeError> {
     let json = json.as_str();
-    dbg!(serde_json::from_str::<Value>(json).unwrap());
+    let _raw = serde_json::from_str::<Value>(json)?;
+    // tracing::info!("raw json: {:#?}", raw);
     let v: ld_schema::LDJSONRoot = match serde_json::from_str(json) {
         Ok(v) => v,
         Err(e) => return Err(ScrapeError::Deserialize(e)),
@@ -92,39 +116,41 @@ fn parse_ld_json(json: String) -> Result<ld_schema::LDJSONRoot, ScrapeError> {
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        let res =
-            crate::scrape_url("https://cooking.nytimes.com/recipes/1015819-chocolate-chip-cookies")
-                .unwrap();
+    #[tokio::test]
+    async fn it_works() {
+        let res = crate::Scraper::new()
+            .scrape_url("https://cooking.nytimes.com/recipes/1015819-chocolate-chip-cookies")
+            .await
+            .unwrap();
         assert_eq!(res.ingredients.len(), 12);
-
-        let result = 2 + 2;
-        assert_eq!(result, 4);
     }
-    #[test]
-    fn it_works_live() {
-        let res = crate::scrape_url("https://diningwithskyler.com/carbone-spicy-rigatoni-vodka/")
+    #[tokio::test]
+    async fn it_works_live() {
+        let res = crate::Scraper::new()
+            .scrape_url("https://diningwithskyler.com/carbone-spicy-rigatoni-vodka/")
+            .await
             .unwrap();
         assert_eq!(res.ingredients.len(), 11);
     }
 
     #[test]
     fn it_works_file() {
-        let res = crate::scrape(
-            include_str!("../test_data/nytimes_chocolate_chip_cookies.html"),
-            "https://cooking.nytimes.com/recipes/1015819-chocolate-chip-cookies",
-        )
-        .unwrap();
+        let res = crate::Scraper::new()
+            .scrape(
+                include_str!("../test_data/nytimes_chocolate_chip_cookies.html"),
+                "https://cooking.nytimes.com/recipes/1015819-chocolate-chip-cookies",
+            )
+            .unwrap();
         assert_eq!(res.ingredients.len(), 12);
     }
     #[test]
     fn it_works_file_se() {
-        let res = crate::scrape(
-            include_str!("../test_data/seriouseats_grilled_naan.html"),
-            "http://www.seriouseats.com/recipes/2011/08/grilled-naan-recipe.html",
-        )
-        .unwrap();
+        let res = crate::Scraper::new()
+            .scrape(
+                include_str!("../test_data/seriouseats_grilled_naan.html"),
+                "http://www.seriouseats.com/recipes/2011/08/grilled-naan-recipe.html",
+            )
+            .unwrap();
         assert_eq!(res.ingredients.len(), 6);
     }
     #[test]
@@ -147,12 +173,24 @@ mod tests {
 
     #[test]
     fn handle_no_ldjson() {
-        let err = crate::scrape(
-            include_str!("../test_data/missing.html"),
-            "https://missing.com",
-        )
-        .unwrap_err();
+        assert!(matches!(
+            crate::Scraper::new()
+                .scrape(
+                    include_str!("../test_data/missing.html"),
+                    "https://missing.com",
+                )
+                .unwrap_err(),
+            crate::ScrapeError::NoLDJSON(_)
+        ));
 
-        assert!(matches!(err, crate::ScrapeError::NoLDJSON(_)));
+        assert!(matches!(
+            crate::Scraper::new()
+                .scrape(
+                    include_str!("../test_data/malformed.html"),
+                    "https://malformed.com",
+                )
+                .unwrap_err(),
+            crate::ScrapeError::NoLDJSON(_)
+        ));
     }
 }
